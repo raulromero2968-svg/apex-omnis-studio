1c78e186dedd6a2545b354ab8cac5b579e29e053
raulromero2968-svg <raulromero2968@gmail.com>
2025-10-27 17:19:52 -0700

    Fix field name mismatch: thumbnailImage -> imageUrl

--- api/portfolio.js
+++ api/portfolio.js
@@ -65,7 +65,7 @@ module.exports = async (req, res) => {
         liveDemo,
         projectUrl,
         documentation,
-        thumbnailImage,
+        imageUrl: thumbnailImage,
         detailImages,
         slug,
       };
